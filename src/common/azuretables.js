52eaa3465b67eeb8694259f5baa3497e2e108285
Konstantin Kostov <me@kvkostov.eu>
2017-03-14 20:24:26 +0100

    store followers in azure table storage

---
+++ src/common/azuretables.js
@@ -0,0 +1,83 @@
+const azure = require('azure-storage');
+const debug = require('debug')('common:azuretables');
+
+/** Creates a table with the given if it doesn't already exist */
+const createTable = (tableName, callback) => {
+  // todo: add retry policy filter
+  const tableSvc = azure.createTableService();
+  tableSvc.createTableIfNotExists(tableName, (error, result, response) => callback(error, result))
+}
+
+/** Adds a JSON object as an entity in a storage table */
+const addEntityToTable = (tableName, entity, callback) => {
+  // todo: add retry policy filter
+  const tableSvc = azure.createTableService();
+
+  // generate a tableentity object from the json payload
+  const entGen = azure.TableUtilities.entityGenerator;
+  let tableEntity = {};
+  for (const prop in entity) {
+    if (entity.hasOwnProperty(prop)) {
+      // todo: check the real type of the value instead of forcing String
+      tableEntity[prop] = entGen.String(`${entity[prop]}`)
+    }
+  }
+  tableSvc.insertOrReplaceEntity(tableName, tableEntity, function(error, result, response) {
+    if (!error) {
+      // Entity inserted
+      callback()
+    } else {
+      callback(error)
+    }
+  });
+}
+
+/** Adds a JSON array of objects as an entities in a storage table */
+const addBatchToTable = (tableName, items, callback) => {
+  // todo: add retry policy filter
+  const tableSvc = azure.createTableService();
+  const entGen = azure.TableUtilities.entityGenerator;
+  let batch = new azure.TableBatch();
+  // azure supports a max of 100 operations per batch
+  const MAX_BATCH = 99;
+  const nextBatch = items.splice(0, MAX_BATCH);
+  const processBatch = (entities) => {
+    for (const entity of entities) {
+      let tableEntity = {};
+      for (const prop in entity) {
+        if (entity.hasOwnProperty(prop)) {
+          // todo: check the real type of the value instead of forcing String
+          tableEntity[prop] = entGen.String(`${entity[prop]}`)
+        }
+      }
+      batch.insertOrReplaceEntity(tableEntity, {
+        echoContent: false
+      })
+    }
+    tableSvc.executeBatch(tableName, batch, function(error, result, response) {
+      if (!error) {
+        // operation successfull
+        if(entities < MAX_BATCH) {
+          // no more items remaining
+          callback()
+        } else {
+          // splice the next portion of items to process
+          const next = items.splice(0, MAX_BATCH);
+          processBatch(next)
+        }
+      } else {
+        callback(error)
+      }
+    });
+  }
+
+  // start the batch, MAX_BATCH items at a time
+  processBatch(nextBatch)
+}
+
+
+module.exports = {
+  createTable,
+  addEntityToTable,
+  addBatchToTable
+}
--- src/twit-followers.js
+++ src/twit-followers.js
@@ -2,10 +2,37 @@
 const util = require('util');
 const debug = require('debug')('workers:twit-followers');
 const twitter = require('./common/twitterclient');
+const azure = require('./common/azuretables');
 
 twitter.getFollowersForUser("kkostov", (err, followers) => {
   if (err) {
     debug(`failed to load followers: ${util.inspect(err, false, null)}`)
   }
   debug(`found ${followers.length} followers`)
+  const formattedFollowers = followers.map(user => {
+    return {
+      PartitionKey: "followers_kkostov",
+      RowKey: `twitter_${user.id_str}`,
+      name: user.name,
+      screen_name: user.screen_name,
+      followers_count: user.followers_count,
+      friends_count: user.friends_count,
+      listed_count: user.listed_count,
+      created_at: user.created_at,
+      favourites_count: user.favourites_count
+    }
+  })
+  azure.createTable('twitfollowers', (error) => {
+    if(error) {
+      debug(`failed to create azure storage table twitfollowers: ${error}`)
+    } else {
+      azure.addBatchToTable('twitfollowers', formattedFollowers, (error) => {
+        if(error) {
+          debug(`failed to insert batch of followers in azure storage table twitfollowers: ${error}`)
+        } else {
+          debug(`followers inserted`)
+        }
+      })
+    }
+  })
 })
