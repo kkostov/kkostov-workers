0a2f1e41236b0f227a52548088722e7a139fa3de
Konstantin Kostov <me@kvkostov.eu>
2017-03-25 16:16:38 +0100

    Twit diff should use promises

--- src/twit-diffusers.js
+++ src/twit-diffusers.js
@@ -2,112 +2,111 @@
 const debug = require('debug')('workers:twit-diffusers');
 const azure = require('./common/azuretables');
 
-const tableName = process.env.AZURE_TABLE_NAME;
-const screenName = process.env.TWITTER_USER;
-
-azure.createTable(tableName, (error) => {
-  if (error) {
-    debug(`failed to create azure storage table ${tableName}: ${error}`)
-    throw error;
-  }
-
-  azure.getEntitiesFromPartition(tableName, `friends_${screenName}`, ['id', 'archived'], (error, friends) => {
-    if (error) {
+const getFriendIds = (tableName, userId) => {
+  return azure.getEntitiesFromPartition(tableName, `friends_${userId}`, ['id', 'archived'])
+    .then(users => users.filter(friend => friend.archived._ !== 'true').map(friend => friend.id._))
+    .catch(error => {
       debug(`failed to load friends from azure table ${tableName}: ${error}`)
       throw error;
-    }
+    });
+}
 
-    azure.getEntitiesFromPartition(tableName, `followers_${screenName}`, ['id', 'archived'], (error, followers) => {
-      if (error) {
-        debug(`failed to load followers from azure table ${tableName}: ${error}`)
-        throw error;
+const getFollowerIds = (tableName, userId) => {
+  return azure.getEntitiesFromPartition(tableName, `followers_${userId}`, ['id', 'archived'])
+    .then(users => users.filter(follower => follower.archived._ !== 'true').map(follower => follower.id._))
+    .catch(error => {
+      debug(`failed to load followers from azure table ${tableName}: ${error}`)
+      throw error;
+    });
+}
+
+
+const getOldDiffs = (tableName, userId) => {
+  const partitionKey = `diffusers_${userId}`;
+  return azure.getEntitiesFromPartition(tableName, '', ['id', 'unfollower', 'fan'])
+    .then(old_diffs => old_diffs.map(od => {
+      return {
+        PartitionKey: partitionKey,
+        RowKey: `${od.id._}`,
+        id: od.id._,
+        unfollower: od.unfollower._,
+        fan: od.fan._
       }
+    }))
+    .catch(error => {
+      debug(`failed to load old diffusers from azure table ${tableName}: ${error}`)
+      throw error;
+    });
+}
 
-      const partitionKey = `diffusers_${screenName}`;
-      const friendIds = friends.filter(friend => friend.archived._ !== 'true').map(friend => friend.id._);
-      const followerIds = followers.filter(follower => follower.archived._ !== 'true').map(follower => follower.id._);
+const syncDiffs = (userId, tableName) => {
+  const partitionKey = `diffusers_${userId}`;
+  return getFriendIds(tableName, userId)
+    .then(friendIds => {
+      getFollowerIds(tableName, userId)
+        .then(followerIds => {
+          getOldDiffs(tableName, userId)
+            .then(oldDiffs => {
+              // filter new unfollowers
+              const unfollowers = friendIds
+                .filter(friendId => followerIds.indexOf(friendId) === -1)
+                .map(twitterUser => {
+                  return {
+                    PartitionKey: partitionKey,
+                    RowKey: `${twitterUser}`,
+                    id: twitterUser,
+                    unfollower: true,
+                    fan: false,
+                    archived: false, // not scheduled for removal
+                    seen: false // new unfollower, the user hasn't dismissed it yet
+                  }
+                });
+              debug(`found ${unfollowers.length} unfollowers`);
 
-      azure.getEntitiesFromPartition(tableName, partitionKey, ['id', 'unfollower', 'fan'], (error, old_diffs) => {
-        if (error) {
-          debug(`failed to load old diffed users from azure table ${tableName}: ${error}`)
-          throw error;
-        }
+              // filter new fans
+              const fans = followerIds
+                .filter(followerId => friendIds.indexOf(followerId) === -1)
+                .map(twitterUser => {
+                  return {
+                    PartitionKey: partitionKey,
+                    RowKey: `${twitterUser}`,
+                    id: twitterUser,
+                    unfollower: false,
+                    fan: true,
+                    archived: false,
+                    seen: false // new unfollower, the user hasn't dismissed it yet
+                  }
+                });
+              debug(`found ${fans.length} fans`);
 
-        // map old diffed users
-        const oldDiffs = old_diffs.map(od => {
-          return {
-            PartitionKey: partitionKey,
-            RowKey: `${od.id._}`,
-            id: od.id._,
-            unfollower: od.unfollower._,
-            fan: od.fan._
-          }
-        });
+              let newDiffs = unfollowers.concat(fans);
+              oldDiffs.forEach(old => {
+                if (!fans.find(fan => fan.id === old.id)) {
+                  // not a fan
+                  old.fan = false;
+                }
 
-        // filter new unfollowers
-        const unfollowers = friendIds
-          .filter(friendId => followerIds.indexOf(friendId) === -1)
-          .map(twitterUser => {
-            return {
-              PartitionKey: partitionKey,
-              RowKey: `${twitterUser}`,
-              id: twitterUser,
-              unfollower: true,
-              fan: false,
-              archived: false, // not scheduled for removal
-              seen: false // new unfollower, the user hasn't dismissed it yet
-            }
-          });
-        debug(`found ${unfollowers.length} unfollowers`);
+                if (!unfollowers.find(unfollower => unfollower.id === old.id)) {
+                  // not an unfollower
+                  old.unfollower = false;
+                }
 
-        // filter new fans
-        const fans = followerIds
-          .filter(followerId => friendIds.indexOf(followerId) === -1)
-          .map(twitterUser => {
-            return {
-              PartitionKey: partitionKey,
-              RowKey: `${twitterUser}`,
-              id: twitterUser,
-              unfollower: false,
-              fan: true,
-              archived: false,
-              seen: false // new unfollower, the user hasn't dismissed it yet
-            }
-          });
-        debug(`found ${fans.length} fans`);
+                // only fans and unfollowers will be kept
+                old.archived = !old.fan && !old.unfollower;
 
+                // since we are merging diff users, only archived ones need updating
+                if (old.archived === true) {
+                  newDiffs.push(old);
+                }
+              })
 
-        let newDiffs = unfollowers.concat(fans);
-
-        // check which old diffs are no longer fans and unfollowers
-        for (let old of oldDiffs) {
-          if (!fans.find(fan => fan.id === old.id)) {
-            // not a fan
-            old.fan = false;
-          }
-
-          if (!unfollowers.find(unfollower => unfollower.id === old.id)) {
-            // not an unfollower
-            old.unfollower = false;
-          }
-
-          // only fans and unfollowers will be kept
-          old.archived = !old.fan && !old.unfollower;
-
-          // since we are merging diff users, only archived ones need updating
-          if(old.archived === true) {
-            newDiffs.push(old);
-          }
-        }
-
-        azure.addBatchToTable(tableName, newDiffs, (error) => {
-          if (error) {
-            debug(`failed to insert batch of diffusers in azure storage table ${tableName}: ${error}`)
-            throw error;
-          }
-        });
-
-      });
+              return azure.addBatchToTable(tableName, newDiffs);
+            })
+        })
     })
-  })
-})
+}
+
+
+module.exports = {
+  syncDiffs
+}
--- src/worker.js
+++ src/worker.js
@@ -1,17 +1,22 @@
 const twitter = require('./common/twitterclient');
+const azure = require('./common/azuretables');
 const followers = require('./twit-followers');
 const friends = require('./twit-friends');
+const differ = require('./twit-diffusers');
 const debug = require('debug')('workers:charles');
 
 const screenName = process.env.TWITTER_USER;
 const tableName = process.env.AZURE_TABLE_NAME;
 
-twitter.getUserInfo(screenName).then(userInfo => {
-  debug(`found user info for ${screenName}, id: ${userInfo.id}`);
-  followers.syncFollowers(userInfo.id, tableName).catch(err => {
-    debug(`error syncing followers: ${err}`);
-  })
-  .then(() => friends.syncFriends(userInfo.id, tableName).catch(err => {
-    debug(`error syncing friends: ${err}`);
-  }));
-});
+azure.createTable(tableName)
+  .catch(err => debug(`failed to create azure table ${tableName}, err: ${err}`))
+  .then(() => twitter.getUserInfo(screenName)
+    .then(userInfo => {
+      debug(`found user info for ${screenName}, id: ${userInfo.id}`);
+      followers.syncFollowers(userInfo.id, tableName)
+        .catch(err => debug(`error syncing followers: ${err}`))
+        .then(() => friends.syncFriends(userInfo.id, tableName)
+          .catch(err => debug(`error syncing friends: ${err}`))
+          .then(() => differ.syncDiffs(userInfo.id, tableName)
+            .catch(err => debug(`error diffing users: ${err}`))));
+    }));
