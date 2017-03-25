7537e94cf4bd12a95ae774a3f1b191ca4028271c
Konstantin Kostov <me@kvkostov.eu>
2017-03-25 14:43:52 +0100

    Twitter client api should use promises

--- src/common/twitterclient.js
+++ src/common/twitterclient.js
@@ -1,5 +1,7 @@
 const Twitter = require('twitter');
 
+const Promise = require('bluebird').Promise;
+
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -7,62 +9,75 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+/** Retrieves information about the twitter user with the given screen name */
+const getUserInfo = (screenName) => {
+  return client.get('users/show', {
+    screen_name: screenName
+  });
+};
 
-/** Returns a list of followers for the given screen name */
-const getFollowersForUser = (screenName, callback) => {
+/** Retrieves the ids of all followers for the given user id */
+const getFollowerIds = (userId) => {
+  return new Promise((resolve, reject) => {
+    const getPagedFollowers = (pageId, lastPageData) => client.get('followers/ids', {
+      user_id: userId,
+      count: 5000,
+      cursor: pageId
+    })
+    .then(data => {
+      let dataPage = data["ids"];
+      // append to previous results
+      if (lastPageData) {
+        dataPage = dataPage.concat(lastPageData)
+      }
 
-  const getPagedFollowers = (pageId, lastPageData) => client.get('followers/ids', {
-    screen_name: screenName,
-    count: 5000,
-    cursor: pageId
-  })
-  .then(data => {
-    let dataPage = data["ids"];
-    if (lastPageData) {
-      dataPage = dataPage.concat(lastPageData)
-    }
-    if (data["next_cursor"] !== 0 && data["next_cursor"] !== "0") {
-      // there are more pages to fetch
-      getPagedFollowers(data["next_cursor"], dataPage)
-    } else {
-      // we have all followers
-      callback(undefined, dataPage)
-    }
-  })
-  .catch(err => callback(err))
+      // check if more pages are available and resume
+      if (data["next_cursor"] !== 0 && data["next_cursor"] !== "0") {
+        // there are more pages to fetch
+        getPagedFollowers(data["next_cursor"], dataPage)
+      } else {
+        // we have all followers
+        resolve(dataPage);
+      }
+    })
+    .catch(err => reject(err))
 
-  getPagedFollowers(-1)
+    // start fetching from the first page
+    getPagedFollowers(-1)
+  });
 }
 
+/** Retrieves the ids of all friends for the given user id */
+const getFriendsIds = (userId) => {
+  return new Promise((resolve, reject) => {
+    const getPagedFriends = (pageId, lastPageData) => client.get('friends/ids', {
+      user_id: userId,
+      count: 5000,
+      cursor: pageId
+    })
+    .then(data => {
+      let dataPage = data["ids"];
+      if (lastPageData) {
+        dataPage = dataPage.concat(lastPageData)
+      }
+      if (data["next_cursor"] !== 0 && data["next_cursor"] !== "0") {
+        // there are more pages to fetch
+        getPagedFriends(data["next_cursor"], dataPage)
+      } else {
+        // we have all followers
+        resolve(dataPage)
+      }
+    })
+    .catch(err => reject(err))
 
-/** Returns a list of friends for the given screen name */
-const getFriendsForUser = (screenName, callback) => {
-
-  const getPagedFriends = (pageId, lastPageData) => client.get('friends/ids', {
-    screen_name: screenName,
-    count: 5000,
-    cursor: pageId
+    // start fetching from the first page
+    getPagedFriends(-1)
   })
-  .then(data => {
-    let dataPage = data["ids"];
-    if (lastPageData) {
-      dataPage = dataPage.concat(lastPageData)
-    }
-    if (data["next_cursor"] !== 0 && data["next_cursor"] !== "0") {
-      // there are more pages to fetch
-      getPagedFriends(data["next_cursor"], dataPage)
-    } else {
-      // we have all followers
-      callback(undefined, dataPage)
-    }
-  })
-  .catch(err => callback(err))
-
-  getPagedFriends(-1)
 }
 
 
 module.exports = {
-  getFollowersForUser,
-  getFriendsForUser
+  getUserInfo,
+  getFollowerIds,
+  getFriendsIds
 }
